8eabd9759ff7dae3451b2d8c746d9faee7415a89
luigi-ortu96 <ortu.salvatore@alice.it>
2016-12-02 09:50:08 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,3 @@
 git config --global user.name luigi-ortu96
-git config --global user.mail ortu.salvatore@alice.it
+git config --global user.email ortu.salvatore@alice.it
 git clone https://github.com/luigi-ortu96/web.git
